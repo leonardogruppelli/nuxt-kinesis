9c6bf3d719720c9f32019fa92d9b636cc244b6eb
Leonardo Gruppelli <leonardo.gruppelli@prodigious.com>
2020-01-10 16:57:25 -0300

    Iniciando efeito parallax.

--- components/parallax.vue
+++ components/parallax.vue
@@ -1,3 +1,7 @@
 <template>
-	<section class="parallax"></section>
+	<section class="parallax">
+		<kinesis-container event="scroll">
+			<kinesis-element tag="div" :strength="50" class="parallax__background" />
+		</kinesis-container>
+	</section>
 </template>
